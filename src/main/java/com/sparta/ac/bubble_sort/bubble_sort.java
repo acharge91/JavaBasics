a1aafe5c6b030b603fa8627a42cd6fa120022578
acharge91 <ashtoncharge@gmail.com>
2022-05-23 17:18:35 +0100

    added bubble sort file with method

---
+++ src/main/java/com/sparta/ac/bubble_sort/bubble_sort.java
@@ -0,0 +1,23 @@
+package com.sparta.ac.bubble_sort;
+
+
+import java.util.Arrays;
+
+public class bubble_sort {
+    public static void main( String[] args ) {
+        int[] numArray = {22, 88, 64, 42, 2, 35, 73};
+        System.out.println(numArray);
+
+        for (int x = 0; x < numArray.length; x++) {
+            for (int y = 0; y < numArray.length - 1; y++) {
+                if (numArray[y] > numArray[y+1] - 1) {
+                    numArray[y] = (numArray[y] + numArray[y+1]) - (numArray[y+1] = numArray[y]);
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(numArray));
+
+
+    }
+}
