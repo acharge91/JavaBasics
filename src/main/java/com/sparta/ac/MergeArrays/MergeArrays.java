4fc3cca8e24fef6b0ffe000636271ae57c2c928f
acharge91 <ashtoncharge@gmail.com>
2022-05-27 18:11:12 +0100

    added more tests for null entries

--- src/main/java/com/sparta/ac/MergeArrays/MergeArrays.java
+++ src/main/java/com/sparta/ac/MergeArrays/MergeArrays.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 public class MergeArrays {
     public static int[] getMergedArray(int[] arrayOne, int[] arrayTwo) {
-        int[] mergedArray = new int[arrayOne.length + arrayTwo.length];
-        int array1Counter = 0;
-        int array2Counter = 0;
-        int indexCount = 0;
 
         if (arrayOne != null && arrayTwo != null) {
+            int[] mergedArray = new int[arrayOne.length + arrayTwo.length];
+            int array1Counter = 0;
+            int array2Counter = 0;
+            int indexCount = 0;
             while (array1Counter < arrayOne.length && array2Counter < arrayTwo.length) {
 
                 if (array1Counter < arrayOne.length && arrayOne[array1Counter] < arrayTwo[array2Counter]) {
@@ -26,15 +26,24 @@ public class MergeArrays {
             while (array2Counter < arrayTwo.length) {
                 mergedArray[indexCount++] = arrayTwo[array2Counter++];
             }
-
+            return mergedArray;
+        } else {
+            if (arrayOne == null && arrayTwo == null) {
+                int[] emptyArray = {};
+                return emptyArray;
+            } else if (arrayOne == null) {
+                return arrayTwo;
+            } else {
+                return arrayOne;
+            }
         }
-        return mergedArray;
+
     }
 
     public static void main(String[] args){
-        int[] arrayOne = {1, 5, 17, 22, 39, 98};
-        int[] arrayTwo = {3, 11, 35, 40, 96};
+        int[] arrayOne;
+        int[] arrayTwo;
 
-        System.out.println(Arrays.toString(getMergedArray(arrayOne, arrayTwo)));
+        System.out.println(Arrays.toString(getMergedArray(null, null)));
     }
 }
--- src/test/java/com/sparta/ac/MergeArrays/MergeArraysTest.java
+++ src/test/java/com/sparta/ac/MergeArrays/MergeArraysTest.java
@@ -25,5 +25,36 @@ public class MergeArraysTest {
         Assertions.assertArrayEquals(expectedArray, MergeArrays.getMergedArray(testArrayOne, testArrayTwo));
     }
 
-    
+    @Test
+    @DisplayName("Test array with negative numbers")
+    void checkNagativeArrayIsMerged() {
+        int[] testArrayOne = {-5, -1, 17, 22, 39, 98};
+        int[] testArrayTwo = {-3, 11, 35, 40, 96};
+        int[] expectedArray = {-5, -3, -1, 11, 17, 22, 35, 39, 40, 96, 98};
+        Assertions.assertArrayEquals(expectedArray, MergeArrays.getMergedArray(testArrayOne, testArrayTwo));
+    }
+
+    @Test
+    @DisplayName("Test arrayone null returns arraytwo")
+    void checkArrayOneNullReturnsArrayTwo() {
+        int[] testArrayTwo = {-3, 11, 35, 40, 96};
+        int[] expectedArray = {-3, 11, 35, 40, 96};
+        Assertions.assertArrayEquals(expectedArray, MergeArrays.getMergedArray(null, testArrayTwo));
+    }
+    @Test
+    @DisplayName("Test arraytwo null returns arrayone")
+    void checkArrayTwoNullReturnsArrayOne() {
+        int[] testArrayOne = {-3, 11, 35, 40, 96};
+        int[] expectedArray = {-3, 11, 35, 40, 96};
+        Assertions.assertArrayEquals(expectedArray, MergeArrays.getMergedArray(testArrayOne, null));
+    }
+
+    @Test
+    @DisplayName("Test both null returns empty array")
+    void checkBothNullReturnsEmptyArray() {
+
+        int[] expectedArray = {};
+        Assertions.assertArrayEquals(expectedArray, MergeArrays.getMergedArray(null, null));
+    }
+
 }
